d9871916440ec7e199decf2fc35639d373caaa5c
Your Namescualy <you@example.compascal.guillen@gmail.com>
2017-02-08 17:39:45 +0100

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,39 @@
+
 import spark.*;
 import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.nio.file.*;
+import static spark.Spark.*;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        get("/", (req, res) ->
+                  "<form method='post' enctype='multipart/form-data'>" // note the enctype
+                + "    <input type='file' name='uploaded_file' accept='.png'>" // make sure to call getPart using the same "name" in the post
+                + "    <button>Upload picture</button>"
+                + "</form>"
+        );
+
+        post("/", (req, res) -> {
+
+            req.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement("/temp"));
+
+            try (InputStream input = req.raw().getPart("uploaded_file").getInputStream()) { // getPart needs to use same "name" as input field in form
+                //Files.copy(input, tempFile, StandardCopyOption.REPLACE_EXISTING);
+            }
+            return "<h1>You uploaded this image:<h1><img src='";
+        });
+
+    }
+}
+
+
+
+/*import spark.*;
+/*import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
 import java.nio.file.*;
@@ -56,3 +90,4 @@ public class Main {
 	});
   }
 }
+*/
