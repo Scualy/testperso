10bb6234885509d7c84ddfbcde6910c17fe61096
Your Namescualy <you@example.compascal.guillen@gmail.com>
2017-02-08 16:11:20 +0100

    correction java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 import javax.servlet.MultipartConfigElement;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
-import javax.servlet.http.HttpServletResponse
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletRegistration;
 import java.io.File;
 import java.io.FileInputStream;
